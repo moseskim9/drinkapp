e1042a04328f6c9c406930e5e2e5b9cc5dd08a95
Moses Kim <moseskim92@yahoo.com>
2016-04-04 16:18:43 +0200

    added name to store model finished the controllers for store and item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,37 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def show
   end
 
-  def add
+  def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
   end
 
   def edit
   end
 
   def update
+    @item.update(item_params)
   end
 
   def destroy
+    @item.destroy
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price)
   end
 end
--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,22 +1,46 @@
 class StoresController < ApplicationController
+  before_action :find_store, only: [:show, :edit, :update, :destroy]
+
   def index
+    @stores = Store.all
+
   end
 
   def show
   end
 
-  def add
+  def new
+    @store = Store.new
   end
 
   def create
+    @store = Store.new(store_params)
+    if @store.save
+      redirect_to store_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @store.update(store_params)
+    @store.save
   end
 
   def destroy
+    @store.destroy
+  end
+
+  private
+
+  def find_store
+    @store = Store.find(params[:id])
+  end
+
+  def store_params
+    params.require(:store).permit(:image, :address, :postcode, :city, :country)
   end
 end
